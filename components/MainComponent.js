9b673ef80b25575b8be6cc3d228f3840ac53da14
Heather <smplisiti@gmail.com>
2020-07-11 16:56:21 -1000

    Favorites

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -5,6 +5,7 @@ import Home from "./HomeComponent";
 import Directory from "./DirectoryComponent";
 import CampsiteInfo from "./CampsiteInfoComponent";
 import Reservation from './ReservationComponent';
+import Favorites from './FavoritesComponent';
 import { View, Platform, StyleSheet, Text, ScrollView, Image } from "react-native";
 import { createStackNavigator, createDrawerNavigator, DrawerItems } from "react-navigation";
 import { Icon } from 'react-native-elements';
@@ -145,6 +146,30 @@ const ReservationNavigator = createStackNavigator(
   }
 );
 
+const FavoritesNavigator = createStackNavigator(
+  {
+      Favorites: { screen: Favorites }
+  },
+  {
+      navigationOptions: ({navigation}) => ({
+          headerStyle: {
+              backgroundColor: '#5637DD'
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+              color: '#fff'
+          },
+          headerLeft: <Icon
+              name='heart'
+              type='font-awesome'
+              iconStyle={styles.stackIcon}
+              onPress={() => navigation.toggleDrawer()}
+          />
+      })
+  }
+);
+
+
 const CustomDrawerContentComponent = props => (
   <ScrollView>
       <SafeAreaView 
@@ -208,6 +233,21 @@ const MainNavigator = createDrawerNavigator(
           )
       }
   },
+
+  Favorites: {
+    screen: FavoritesNavigator,
+    navigationOptions: {
+        drawerLabel: 'My Favorites',
+        drawerIcon: ({tintColor}) => (
+            <Icon
+                name='heart'
+                type='font-awesome'
+                size={24}
+                color={tintColor}
+            />
+        )
+    }
+},
 
     About: { 
       screen: AboutNavigator,
